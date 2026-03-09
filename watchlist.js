9c4070b3d82ad5e1eee40349f6d484dd435a2ce8
Yvette Hernandez <yhern006@users.noreply.github.com>
2026-03-08 20:01:04 -0700

    user watchlist appears

--- watchlist.js
+++ watchlist.js
@@ -1,11 +1,32 @@
-import { displayResults } from "./display.js"
+import { getFromStorage, displayResults } from "./display.js"
 
 const userWatchlistEl = document.getElementById('user-watchlist')
 let userWatchlist = []
-const watchlistLocalStorage = JSON.parse(localStorage.getItem('myWatchlist'))
+const watchlistLocalStorage = getFromStorage()
 
-function getLocalStorage() {
-    userWatchlist = localStorage.getItem('myWatchlist') 
+if(watchlistLocalStorage)
+    userWatchlist = watchlistLocalStorage
+
+console.log(userWatchlist)
+
+fetchFilmsByIds()
+    .then(data => {
+        console.log(data)
+        
+        userWatchlistEl.innerHTML = `
+            <ul>
+                ${displayResults(data)}
+            </ul>
+        `
+    })
+
+async function fetchFilmsByIds() {
+    let films = []
+
+    for(let id of userWatchlist) {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=769e31a9&i=${id}`)
+        const data = await response.json()
+        films.push(data)
+    }
+    return films
 }
-
-displayResults(userWatchlistEl, userWatchlist)
